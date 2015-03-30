4175465b215d66c3a2c199a27caf3abb45adba92
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2015-03-30 21:44:04 +0200

    maffilter (back to version 1.1.0)

--- maffilter.rb
+++ maffilter.rb
@@ -1,7 +1,7 @@
 class Maffilter < Formula
   homepage "http://bioweb.me/maffiter"
-  url "http://biopp.univ-montp2.fr/repos/sources/maffilter/MafFilter-1.0.0.tar.gz"
-  sha256 "935d117340207beef42b74e72dd803b377bf62b41927cdd13d7cc667870f0760"
+  url "http://biopp.univ-montp2.fr/repos/sources/maffilter/maffilter-1.1.0.tar.gz"
+  sha256 "c9ee39981766d3ac339df4b269dd57f39240a5597f1e527da030b80b85caa935"
 
   depends_on "cmake" => :build
   depends_on "libbpp-phyl-omics"
