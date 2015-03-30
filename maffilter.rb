fed1ce1386ac13515cff8e802f7a1f624af206f1
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2015-03-30 21:15:26 +0200

    maffilter (fixed version number)

--- maffilter.rb
+++ maffilter.rb
@@ -1,14 +1,16 @@
 class Maffilter < Formula
   homepage "http://bioweb.me/maffiter"
-  url "http://biopp.univ-montp2.fr/repos/sources/maffilter/maffilter-1.1.0.tar.gz"
+  url "http://biopp.univ-montp2.fr/repos/sources/maffilter/maffilter-1.0.0.tar.gz"
   sha256 "c9ee39981766d3ac339df4b269dd57f39240a5597f1e527da030b80b85caa935"
 
   depends_on "cmake" => :build
   depends_on "libbpp-phyl-omics"
   depends_on "boost"
+  depends_on "zlib"
+  depends_on "bzip2"
  
   def install
-    system "cmake", ".", *std_cmake_args, "-DBUILD_TESTING=no"
+    system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
 
