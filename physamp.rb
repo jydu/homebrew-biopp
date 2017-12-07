1d15ca0ce3e9dac4639bcb2554a2136b91f53f0f
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2017-12-07 21:03:54 +0100

    Updated sha.

--- physamp.rb
+++ physamp.rb
@@ -1,7 +1,7 @@
 class Physamp < Formula
   homepage "http://bioweb.me/physamp"
   url "https://github.com/jydu/physamp/archive/v1.0.1.tar.gz"
-  sha256 "1631bb2af9273c8830f1df4c02ca561c6a03bcc29e5d37074e2d3c26ae557111"
+  sha256 "60ab34b0bde5131b380da719c06c29e71cbe326352d837450e2d9ac611fdd1cb"
 
   depends_on "cmake" => :build
   depends_on "libbpp-phyl"
