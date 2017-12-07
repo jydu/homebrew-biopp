117bc586e47cecb1fd8b971218682abcf05cfab6
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2017-12-07 10:57:22 +0100

    Fixed formula.

--- libbpp-qt.rb
+++ libbpp-qt.rb
@@ -2,8 +2,8 @@ class LibbppQt < Formula
   desc "Bio++ Sequence Library"
   homepage "http://bioweb.me/biopp"
   url "https://github.com/BioPP/bpp-qt/releases/tag/v2.3.1"
-  sha256 "4f7c659970c3d1c2995a2584b50851b81468c5246622c69e725997574a605da6"
-  
+  sha256 "3c1df854bdd694014156c37c0f84930230143e97a089cd065230d127191435c7"
+ 
   depends_on "cmake" => :build
   depends_on "qt"
   depends_on "libbpp-phyl"
