2ad662032a3923b22f76d916f4047f8f5468d959
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2017-12-07 09:39:01 +0100

    Fixed sha again.

--- maffilter.rb
+++ maffilter.rb
@@ -1,7 +1,8 @@
 class Maffilter < Formula
   homepage "http://bioweb.me/maffiter"
   url "https://github.com/jydu/maffilter/releases/tag/v1.2.1"
-  sha256 "b1f4a01712bf0f1be9810f6cd221a3c6732c8e3d0e7288da0d88025c7c1d69e2"
+  sha256 "d6b3604a34ea8bf097054e0f9b449ceae010df238c8760e884b5184dd6f2a28e"
+
   depends_on "cmake" => :build
   depends_on "libbpp-phyl-omics"
   depends_on "boost"
