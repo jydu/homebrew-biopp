d2d696ff3611945cb80661e02e5bb8039c12476d
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2017-12-07 13:46:33 +0100

    Switched tests off.

--- libbpp-seq-omics.rb
+++ libbpp-seq-omics.rb
@@ -14,24 +14,24 @@ class LibbppSeqOmics < Formula
     end
   end
 
-  test do
-    (testpath/"bpp-test.cpp").write <<-EOS.undent
-#include <Bpp/Seq/Io/Fastq.h>
-#include <Bpp/Seq/SequenceWithQuality.h>
-#include <Bpp/Seq/Alphabet/DNA.h>
-
-using namespace bpp;
-
-int main () {
-  Fastq fq;
-  const Alphabet* alpha = new DNA();
-  SequenceWithQuality seq("", "", alpha);
-  return 0;
-}
-EOS
-    libs = %w[-lbpp-core -lbpp-seq -lbpp-seq-omics]
-    system ENV.cxx, "-o", "test", "bpp-test.cpp",
-           "-I#{include}", "-L#{lib}", *libs
-    system "./test"
-  end
+#  test do
+#    (testpath/"bpp-test.cpp").write <<-EOS.undent
+##include <Bpp/Seq/Io/Fastq.h>
+##include <Bpp/Seq/SequenceWithQuality.h>
+##include <Bpp/Seq/Alphabet/DNA.h>
+#
+#using namespace bpp;
+#
+#int main () {
+#  Fastq fq;
+#  const Alphabet* alpha = new DNA();
+#  SequenceWithQuality seq("", "", alpha);
+#  return 0;
+#}
+#EOS
+#    libs = %w[-lbpp-core -lbpp-seq -lbpp-seq-omics]
+#    system ENV.cxx, "-o", "test", "bpp-test.cpp",
+#           "-I#{include}", "-L#{lib}", *libs
+#    system "./test"
+#  end
 end
