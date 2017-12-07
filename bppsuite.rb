6558a7140cf67792202be03531ff52d41c6b0d6b
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2017-12-07 10:32:34 +0100

    Fixed sha.

--- bppsuite.rb
+++ bppsuite.rb
@@ -2,7 +2,7 @@ class Bppsuite < Formula
   desc "The Bio++ Program Suite"
   homepage "http://bioweb.me/biopp"
   url "https://github.com/BioPP/bppsuite/releases/tag/v2.3.1"
-  sha256 "4a5075d0c21f5ee880d31813349bebcdb5033250b7c364d578ddba1ff1a75f98"
+  sha256 "cb93ff5d470ad0284bb8fb2eca67783d0afd4c91b22aa3a74450af6c6d28f538"
   depends_on "cmake" => :build
   depends_on "libbpp-phyl"
   depends_on "libbpp-popgen"
