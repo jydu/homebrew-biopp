00df34cab7006be897851eaadec0ac21797381d5
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2017-12-07 15:12:22 +0100

    Fixed formula.

--- maffilter.rb
+++ maffilter.rb
@@ -1,6 +1,6 @@
 class Maffilter < Formula
   homepage "http://bioweb.me/maffiter"
-  url "https://github.com/jydu/maffilter/releases/tag/v1.2.1"
+  url "https://github.com/jydu/maffilter/archive/v1.2.1.tar.gz"
   sha256 "d6b3604a34ea8bf097054e0f9b449ceae010df238c8760e884b5184dd6f2a28e"
 
   depends_on "cmake" => :build
