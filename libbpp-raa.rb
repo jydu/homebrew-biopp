39ebafe9eb43f907b15000a4b58d9871d31bebc5
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2017-12-07 10:51:39 +0100

    Fixed bpp-raa formula and sha.

--- libbpp-raa.rb
+++ libbpp-raa.rb
@@ -2,8 +2,8 @@ class LibbppRaa < Formula
   desc "Bio++ RAA Library"
   homepage "http://bioweb.me/biopp"
   url "https://github.com/BioPP/bpp-raa/archive/v2.3.1.tar.gz"
-  sha256 "c7ec73a5af84808362f301479c548b6a01c47a66065b28a053ff8043409e861a"
-
+  sha256 "6abcb159d22d88927c40182f3cf40285e7defae41fd66cafa45310d5fd3cf5be"
+  
   depends_on "cmake" => :build
   depends_on "libzip"
   depends_on "libbpp-seq"
@@ -11,7 +11,7 @@ class LibbppRaa < Formula
   def install
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
-      system "make", "bpp-seq-omics-shared", "#bpp-seq-omics-static"
+      system "make", "bpp-raa-shared", "bpp-raa-static"
       system "make", "install"
     end
   end
