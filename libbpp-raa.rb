18392698f5107b47392831c97b322a8c38840fcd
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2017-12-07 11:02:04 +0100

    Another sha fix.

--- libbpp-raa.rb
+++ libbpp-raa.rb
@@ -2,7 +2,7 @@ class LibbppRaa < Formula
   desc "Bio++ RAA Library"
   homepage "http://bioweb.me/biopp"
   url "https://github.com/BioPP/bpp-raa/archive/v2.3.1.tar.gz"
-  sha256 "6abcb159d22d88927c40182f3cf40285e7defae41fd66cafa45310d5fd3cf5be"
+  sha256 "3fa94695c76d88fb3d28ebaa2bffcb688f59d302874ea9aa62e6e88f8544b06c"
   
   depends_on "cmake" => :build
   depends_on "libzip"
