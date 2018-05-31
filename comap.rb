e3cf269c08e10d0cc421eee446a764c206f84b21
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2018-05-31 09:53:11 +0200

    Updated sha.

--- comap.rb
+++ comap.rb
@@ -1,7 +1,7 @@
 class Comap < Formula
   homepage "http://bioweb.me/comap"
   url "https://github.com/jydu/comap/archive/v1.5.5.tar.gz"
-  sha256 "d47a83176a6fca9b00052dd877d0f719a1baeb833e16b1efa23958e4fe43e369"
+  sha256 "97b82a38c06541bbdefc31308cfee258afd0a989ad192389b3171db8b5c809d0"
   depends_on "cmake" => :build
   depends_on "libbpp-phyl"
 
