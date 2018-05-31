c541aea0a81a6e460560175e1700461ca7ad052e
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2018-05-31 09:51:56 +0200

    New comap version 1.5.5.

--- comap.rb
+++ comap.rb
@@ -1,6 +1,6 @@
 class Comap < Formula
   homepage "http://bioweb.me/comap"
-  url "https://github.com/jydu/comap/archive/v1.5.4.tar.gz"
+  url "https://github.com/jydu/comap/archive/v1.5.5.tar.gz"
   sha256 "d47a83176a6fca9b00052dd877d0f719a1baeb833e16b1efa23958e4fe43e369"
   depends_on "cmake" => :build
   depends_on "libbpp-phyl"
