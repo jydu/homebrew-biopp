2dce6ff6ca88851a1a69d336f20082c0c2648bd5
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2018-11-08 12:04:10 +0100

    Updated sha256.

--- libbpp-qt.rb
+++ libbpp-qt.rb
@@ -2,7 +2,7 @@ class LibbppQt < Formula
   desc "Bio++ Sequence Library"
   homepage "http://bioweb.me/biopp"
   url "https://github.com/BioPP/bpp-qt/archive/v2.4.1.tar.gz"
-  sha256 "281853156b2ed331975de15c88e139583ca92d300bbd34cd3f1b61ab5e42e269"
+  sha256 "4423b35223415c6dc6b483df3cf3276dafabd93c04dfc4716ce0b8e5b3ea219b"
  
   depends_on "cmake" => :build
   depends_on "qt"
